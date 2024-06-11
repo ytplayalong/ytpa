c1c6a888b4bfeb7698c6bebfafd90d0ea4de8c9b
Christian Baumann <chbauman@student.ethz.ch>
2024-06-11 22:19:46 +0200

    Init react app

---
+++ react/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>YouTube Play-Along</h1>
+      </header>
+      <p>Play along YouTube videos.</p>
+    </div>
+  );
+}
+
+export default App;
