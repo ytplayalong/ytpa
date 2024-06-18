d2ee899e95a88ffd29b136b1e10650e15e23dc85
Christian Baumann <chbauman@student.ethz.ch>
2024-06-18 10:37:40 +0200

    Added cookie Banner

---
+++ next/app/components/cookie.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import cookie from "js-cookie";
+
+const CookieConsentBanner = () => {
+  const [showBanner, setShowBanner] = useState(false);
+
+  useEffect(() => {
+    const consentCookie = cookie.get("cookieConsent");
+    if (!consentCookie) {
+      setShowBanner(true);
+    }
+  }, []);
+
+  const handleAccept = () => {
+    setShowBanner(false);
+    cookie.set("cookieConsent", "accepted", { expires: 365 });
+  };
+
+  if (!showBanner) {
+    return null;
+  }
+
+  const bannerStyle: any = {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    left: 0,
+    right: 0,
+    backgroundColor: "#ffffffee",
+    backgroundSize: "100%",
+    paddingTop: "5em",
+  };
+  return (
+    <div style={bannerStyle}>
+      <div className="container">
+        <p>This website uses cookies to improve your browsing experience.</p>
+        <p>Please click Accept to continue.</p>
+        <button onClick={handleAccept}>Accept</button>
+      </div>
+    </div>
+  );
+};
+
+export default CookieConsentBanner;
--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../app/globals.css";
 import NavigationBar from "./components/navbar";
+import CookieConsentBanner from "./components/cookie";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className} style={{ margin: 0, padding: 0 }}>
+        <CookieConsentBanner />
         <NavigationBar />
         <div className="container">{children}</div>
       </body>
