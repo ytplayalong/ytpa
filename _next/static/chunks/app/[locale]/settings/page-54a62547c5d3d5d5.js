447cb5bd1083a3338e360e21ee47297a25559e6a
Christian Baumann <chbauman@student.ethz.ch>
2024-06-19 13:00:04 +0200

    Updates

---
+++ _next/static/chunks/app/[locale]/settings/page-54a62547c5d3d5d5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[955],{6472:function(n,e,u){Promise.resolve().then(u.bind(u,2635))}},function(n){n.O(0,[389,138,666,971,23,744],function(){return n(n.s=6472)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/layout-8cf3ba8034111285.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{2614:function(n,e,u){Promise.resolve().then(u.t.bind(u,6857,23))},6857:function(){}},function(n){n.O(0,[404,971,23,744],function(){return n(n.s=2614)}),_N_E=n.O()}]);
