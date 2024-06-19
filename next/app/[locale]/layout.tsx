66a8d13aea689358576a5ab3765b881932fe38a6
Christian Baumann <chbauman@student.ethz.ch>
2024-06-19 13:54:18 +0200

    Added open graph tags

--- next/app/[locale]/layout.tsx
+++ next/app/[locale]/layout.tsx
@@ -5,9 +5,31 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+const baseData = {
   title: "YouTube Play-Along",
-  description: "YouTube Play-Along Web Application",
+  description:
+    "YouTube Play-Along Web Application. Sheet music synchronized with YouTube videos.",
+};
+const imgUrl =
+  "https://raw.githubusercontent.com/ytplayalong/ytpa/b10db2fb142978d9126ca9c7869aaba4a2fd9479/next/public/Logo_120_63.png";
+
+export const metadata: Metadata = {
+  ...baseData,
+  openGraph: {
+    ...baseData,
+    url: "https://ytpa.ch",
+    siteName: "YTPA",
+    type: "website",
+    images: [
+      {
+        url: imgUrl,
+        secureUrl: imgUrl,
+        width: 1200,
+        height: 630,
+        alt: "Preview image for YTPA",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
