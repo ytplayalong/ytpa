6be1e9bef4fc07995aed6b785a15f655fafd5df0
Christian Baumann <chbauman@student.ethz.ch>
2024-10-13 22:08:10 +0200

    Manual update

---
+++ _next/static/chunks/app/[locale]/settings/page-9ecfa9f4e4b12efb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[955],{6472:function(n,e,u){Promise.resolve().then(u.bind(u,4267))}},function(n){n.O(0,[822,648,209,971,117,744],function(){return n(n.s=6472)}),_N_E=n.O()}]);
