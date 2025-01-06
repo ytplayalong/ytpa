4f56fce2cf231b6712fe93bd83cd87534eebee7f
Christian Baumann <chbauman@student.ethz.ch>
2025-01-06 22:37:37 +0100

    Update eslint config

--- next/eslint.config.mjs
+++ next/eslint.config.mjs
@@ -1,11 +1,8 @@
-import { fixupPluginRules } from "@eslint/compat";
-import _import from "eslint-plugin-import";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
   {
     plugins: {
-      import: fixupPluginRules(_import),
       "simple-import-sort": simpleImportSort,
     },
 
